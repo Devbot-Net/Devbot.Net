7b5869b2d986a14818250a65e5a10428ff81cc69
Tommy Long <pete_201@hotmail.com>
2015-01-15 02:25:18 +0000

    Package.json added to root to provide grunt-cli for Korebuild.  Directories for nodejs created for Korebuild before build.  Nuget.exe installed if not present/cached.  Unnecessary (commented) .Net install removed.

--- build.cmd
+++ build.cmd
@@ -6,30 +6,42 @@ SET KRE_ARCH=x86
 SET KRE_CLR=CLR
 SET PROJECT_PATH=src\Devbot.Net.Identity
 
-:: 2. Install KVM
+@echo off
+cd %~dp0
+
+SETLOCAL
+SET CACHED_NUGET=%LocalAppData%\NuGet\NuGet.exe
+
+:: 2. Install Nuget to Cache
+IF EXIST %CACHED_NUGET% GOTO copyNuget
+echo Downloading latest version of NuGet.exe...
+IF NOT EXIST %LocalAppData%\NuGet md %LocalAppData%\NuGet
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest 'https://www.nuget.org/nuget.exe' -OutFile '%CACHED_NUGET%'"
+
+:: 3. Copy Nuget from Cache to Local
+:copyNuget
+IF EXIST .nuget\nuget.exe goto kvmInstall
+md .nuget
+copy %CACHED_NUGET% .nuget\nuget.exe > nul
+
+:: 4. Install KVM
+:kvmInstall
 Start /w PowerShell -NoProfile -NoLogo -ExecutionPolicy unrestricted -Command "[System.Threading.Thread]::CurrentThread.CurrentCulture = ''; [System.Threading.Thread]::CurrentThread.CurrentUICulture = '';& '%~dp0build\build.ps1' '%~dp0'"
 
-:: 3. Install KRE
+:: 5. Restore Build Components
+IF EXIST packages\KoreBuild goto runBuild
+.nuget\NuGet.exe install KoreBuild -version 0.2.1-rc1-10068 -ExcludeVersion -o packages -nocache -pre
+.nuget\NuGet.exe install Sake -version 0.2 -o packages -ExcludeVersion
+
+:: 6. Install KRE
 SET KRE_HOME=%USERPROFILE%\.kre
 CALL %KRE_HOME%\bin\kvm install %KRE_VERSION% -%KRE_ARCH% -r %KRE_CLR%
 
-:: 4. Build Solution
-SET KRE_CHOSEN=KRE-%KRE_CLR%-%KRE_ARCH%.%KRE_VERSION%
-SET KPM_PATH=%KRE_HOME%\packages\%KRE_CHOSEN%
-CALL %KPM_PATH%\bin\kpm build "%~dp0%PROJECT_PATH%"
+:: 7. Create Root Nodejs Directory
+:runBuild
+IF NOT EXIST bin mkdir bin
+IF NOT EXIST bin\nodejs mkdir bin\nodejs
 
-GOTO end
-
-:error
-ECHO An error has occurred during build
-call :exitSetErrorLevel
-call :exitFromFunction 2>nul
-
-:exitSetErrorLevel
-exit /b 1
- 
-:exitFromFunction
-()
-
-:end
-echo Finished successfully.
+:: 8. Run Build
+CALL %KRE_HOME%\bin\kvm use  %KRE_VERSION% -%KRE_ARCH% -r %KRE_CLR%
+packages\Sake\tools\Sake.exe -I packages\KoreBuild\build -f build\makefile.shade %*
